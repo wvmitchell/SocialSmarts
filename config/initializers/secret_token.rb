70aefbe8a18671eaa817e1ea31e1a8d13b221b5a
Will Mitchell <wvmitchell@gmail.com>
2014-01-06 15:20:12 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialSmarts::Application.config.secret_key_base = 'cb69176353152e4891c5851d273af3ce978ad1356bc741dc7dea6b409d16fcfa66cdf97b250818f9e0ae59536809314013186f536ce4e5dd83497efcacd2e391'
