6981704cc08e635edddc03bad17da2b20f38946a
Will Mitchell <wvmitchell@gmail.com>
2014-01-07 15:03:56 -0700

    Gathering tweets in progress

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,11 @@ class DashboardController < ApplicationController
 
   def index
     redirect_to login_path unless current_user
+    if current_user
+      raise current_user.nickname
+      tweeter = TweetFetcher.new
+      @tweets = tweeter.find_tweets_mentioning(current_user.nickname)
+    end
   end
+
 end
