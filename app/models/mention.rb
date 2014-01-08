485ec32479c1a6cba7d4d8ec8b76ca2d05edd237
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-01-08 16:08:30 -0700

    Store mentions, add validation for mention uniqueness

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,2 +1,22 @@
+require 'digest'
+
 class Mention < ActiveRecord::Base
+  belongs_to :user
+  validates_uniqueness_of :hash_id
+
+  def self.add_tweets_for(user)
+    tf = TweetFetcher.new
+    kf = KloutFetcher.new
+    tweets = tf.find_tweets_mentioning(user)
+    tweets.each do |tweet|
+      m = Mention.new
+      m.username = tweet.user.username
+      m.message = tweet.text
+      m.klout = kf.get_score_for(user)
+      m.hash_id = Digest::SHA1.hexdigest(m.username + m.message)
+      m.tweet_timestamp = tweet.created_at
+      m.save
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :mentions
+
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
