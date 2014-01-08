a57861a041a67d9db32d1725bb75a332b3052b33
Will Mitchell <wvmitchell@gmail.com>
2014-01-07 17:51:57 -0700

    Patch to deal with tweeters that have no klout

--- app/models/klout_fetcher.rb
+++ app/models/klout_fetcher.rb
@@ -1,17 +1,21 @@
+require 'pry'
+
 class KloutFetcher
 
   BASE_URL = "http://api.klout.com/v2/"
 
   def get_score_information_for(user)
     response = Faraday.get(BASE_URL + "identity.json/twitter?screenName=#{user}&key=#{ENV['KLOUT_KEY']}")
-    id = JSON.parse(response.body)["id"]
-    second_response = Faraday.get(BASE_URL + "user.json/#{id}/score?key=#{ENV['KLOUT_KEY']}")
-    JSON.parse(second_response.body)
+    unless response.body == ""
+      id = JSON.parse(response.body)["id"]
+      second_response = Faraday.get(BASE_URL + "user.json/#{id}/score?key=#{ENV['KLOUT_KEY']}")
+      JSON.parse(second_response.body)
+    end
   end
 
   def get_score_for(user)
     info = get_score_information_for(user)
-    info["score"]
+    info["score"] if info
   end
 
 end
