fe090e3f39eee7e66ddb8bbdf629570e0c9ca1e8
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-01-14 13:25:16 -0700

    Update dashboard index controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
 
   def index
     redirect_to login_path unless current_user
-    @mentions = current_user.mentions
+    # @mentions = current_user.mentions
     if current_user
       Mention.add_tweets_for(current_user)
       @mentions = Mention.get_latest_mentions_for(current_user)
