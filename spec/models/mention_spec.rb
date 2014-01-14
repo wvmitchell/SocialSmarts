2f35f84eadda2c0e72370e51fbecf96f4c3e2ced
Antony Siegert <agsiegert@hotmail.com>
2014-01-14 14:55:57 -0700

    archived button working, test error

--- spec/models/mention_spec.rb
+++ spec/models/mention_spec.rb
@@ -7,4 +7,12 @@ require 'spec_helper'
       mention.send_to_archived
       mention.archived.should eq(true)
     end
+
+    it "can retrieve both archived and unarchived mentions", :vcr do
+      user = FactoryGirl.build(:user, name: "jsl_demo_07")
+      fetcher = TweetFetcher.new(user)
+      mention = FactoryGirl.build(:mention, user_id: user.id)
+      mentions = Mention.get_unarchived_mentions_for(user)
+      mentions.should include("@weesie_b This is a Tweet")
+    end
   end
