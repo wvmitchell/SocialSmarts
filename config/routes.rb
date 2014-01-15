b5a2aac12114dcb1cb990df156b3c14955843731
George Hudson <georgehudson78@gmail.com>
2014-01-15 16:03:22 -0700

    added api controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SocialSmarts::Application.routes.draw do
 
-  namespace :api do
+  namespace :api, :path => "", :constraints => {:subdomain => "api"}, :defaults => {:format => :json} do
     namespace :v1 do
       resources :mentions, only:[:index, :show]
     end
