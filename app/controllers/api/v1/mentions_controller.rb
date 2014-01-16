55385c77a49b687570aeee61cf9514856995eb4a
George Hudson <georgehudson78@gmail.com>
2014-01-16 10:54:33 -0700

    build api index and archive mentions

--- app/controllers/api/v1/mentions_controller.rb
+++ app/controllers/api/v1/mentions_controller.rb
@@ -1,2 +1,10 @@
 class API::V1::MentionsController < ApplicationController
+  respond_to :json
+  def index
+    respond_with Mention.limit(5).order("created_at DESC")
+  end
+
+  def send_to_archived
+    respond_with Mention.archived = true
+  end
 end
