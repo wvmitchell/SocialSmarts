6e5c86200492bf4c55cbfa9ec79263525fa9ea90
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2014-01-21 19:44:56 -0700

    Add an update_klout method to Klout controller

--- app/controllers/api/v1/klout_controller.rb
+++ app/controllers/api/v1/klout_controller.rb
@@ -2,6 +2,19 @@ class API::V1::KloutController < ApplicationController
   respond_to :json
   
   def index
-    respond_with KloutFetcher.new.get_score_information_for(params[:user])
+    respond_with Mention.where(username: params[:user]).first.klout
+  end
+
+  def update_klout
+    mention = Mention.where(username: params[:user]).first
+    mention.klout = params[:score]
+    mention.save
+    respond_to do |format|
+      if mention.save
+        format.json { render json: mention, status: :created }
+      else
+        format.json { render json: mention.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
