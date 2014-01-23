c510112e17e5b99dbf7eca411431650b61074c99
Antony Siegert <agsiegert@hotmail.com>
2014-01-22 19:59:58 -0700

    update gemfile, kloutfetcherworker

--- app/Jobs/klout_fetcher_worker.rb
+++ app/Jobs/klout_fetcher_worker.rb
@@ -4,11 +4,9 @@ class KloutFetcherWorker
 
 
   def self.perform(mention_id)
-    mentions = Mention.where(mention_id:, mention_id)
-    mentions.each do |mention|
-      mention.update_klout
-      sleep(1)
-    end
+    mention = Mention.find(mention_id)
+    mention.update_klout
+    sleep(1)
   end
 
 end
