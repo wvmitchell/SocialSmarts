45f50d1582df1667d031895298beb3d9c1ca54b2
Will Mitchell <wvmitchell@gmail.com>
2014-01-22 19:52:00 -0700

    removed sqlite, again

--- app/Jobs/klout_fetcher_worker.rb
+++ app/Jobs/klout_fetcher_worker.rb
@@ -2,9 +2,8 @@ class KloutFetcherWorker
 
   @queue = :klout_fetch
 
-
   def self.perform(mention_id)
-    mentions = Mention.where(mention_id:, mention_id)
+    mentions = Mention.where(mention_id: mention_id)
     mentions.each do |mention|
       mention.update_klout
       sleep(1)
